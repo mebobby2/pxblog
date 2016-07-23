2bce1a6cf9a104a97d07a9e87329b4f3ac35a452
Bobby Lei <bobl53@gmail.com>
2016-07-23 13:44:10 +0800

    router for users added

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,9 @@ defmodule Pxblog.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
     resources "posts", PostController
+    resources "users", UserController
   end
 
   # Other scopes may use custom stacks.
