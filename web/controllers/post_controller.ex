72f50d2bce9bfa1fef2229c90e3a6c09ac3cc1ae
Bobby Lei <bobl53@gmail.com>
2016-07-26 22:12:34 +0800

    updated post controller to use the new user-post assoc

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -6,17 +6,23 @@ defmodule Pxblog.PostController do
   plug :assign_user
 
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Repo.all(assoc(conn.assigns[:user], :posts))
     render(conn, "index.html", posts: posts)
   end
 
   def new(conn, _params) do
-    changeset = Post.changeset(%Post{})
+    changeset = 
+      conn.assigns[:user]
+      |> build_assoc(:posts)
+      |> Post.changeset()
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"post" => post_params}) do
-    changeset = Post.changeset(%Post{}, post_params)
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:posts)
+      |> Post.changeset(post_params)
 
     case Repo.insert(changeset) do
       {:ok, _post} ->
@@ -29,18 +35,18 @@ defmodule Pxblog.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     render(conn, "show.html", post: post)
   end
 
   def edit(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     changeset = Post.changeset(post)
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     changeset = Post.changeset(post, post_params)
 
     case Repo.update(changeset) do
@@ -54,7 +60,7 @@ defmodule Pxblog.PostController do
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -68,10 +74,18 @@ defmodule Pxblog.PostController do
   defp assign_user(conn, _opts) do
     case conn.params do
       %{"user_id" => user_id} ->
-        user = Repo.get(Pxblog.User, user_id)
-        assign(conn, :user, user)
-      _ ->
-        conn
+        case Repo.get(Pxblog.User, user_id) do
+          nil -> invalid_user(conn)
+          user -> assign(conn, :user, user)
+        end
+      _ -> invalid_user(conn)
     end
+  end
+
+  defp invalid_user(conn) do
+    conn
+    |> put_flash(:error, "invalid_user!")
+    |> redirect(to: page_path(conn, :index))
+    |> halt
   end
 end
