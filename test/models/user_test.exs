8a9e030e478faa18f4bd36956483cd9a0551b703
Bobby Lei <bobl53@gmail.com>
2016-07-28 13:11:17 +0800

    fixing all tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -1,13 +1,23 @@
 defmodule Pxblog.UserTest do
   use Pxblog.ModelCase
 
+  alias Pxblog.TestHelper
   alias Pxblog.User
 
   @valid_attrs %{email: "some content", password: "test1234", password_confirmation: "test1234", username: "some content"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
+  setup do
+    {:ok, role} = TestHelper.create_role(%{name: "user", admin: false})
+    {:ok, role: role}
+  end
+
+  defp valid_attrs(role) do
+    Map.put(@valid_attrs, :role_id, role.id)
+  end
+
+  test "changeset with valid attributes", %{role: role} do
+    changeset = User.changeset(%User{}, valid_attrs(role))
     assert changeset.valid?
   end
 
