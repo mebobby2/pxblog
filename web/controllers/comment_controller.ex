67fc11ca4fc1a26329b65d016a274bde6735fa45
Bobby Lei <bobl53@gmail.com>
2016-08-06 14:46:40 +0800

    added method to delete comments

--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -23,5 +23,12 @@ defmodule Pxblog.CommentController do
   end
 
   def update(conn, _), do: conn
-  def delete(conn, _), do: conn
+
+  def delete(conn, %{"id" => id, "post_id" => post_id}) do
+    post = Repo.get!(Post, post_id) |> Repo.preload(:user)
+    Repo.get!(Comment, id) |> Repo.delete!
+    conn
+    |> put_flash(:info, "Deleted comment!")
+    |> redirect(to: user_post_path(conn, :show, post.user, post))
+  end
 end
