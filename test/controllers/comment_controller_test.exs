abff32a680f53213b7511308bf9c726ae451c66b
Bobby Lei <bobl53@gmail.com>
2016-08-18 21:51:06 +0800

    authorising delete and updating of comments

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -25,10 +25,16 @@ defmodule Pxblog.CommentControllerTest do
     assert html_response(conn, 200) =~ "Oops, something went wrong"
   end
 
-  test "deletes the comment", %{ conn: conn, post: post, comment: comment } do
-    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+  test "deletes the comment when logged in as an authorized user", %{ conn: conn, post: post, comment: comment, user: user } do
+    conn = login_user(conn, user) |> delete(post_comment_path(conn, :delete, post, comment))
     assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
     refute Repo.get(Comment, comment.id)
+  end
+
+  test "does not delete the comment when not logged in as an authorized user", %{conn: conn, post: post, comment: comment} do
+    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == page_path(conn, :index)
+    assert Repo.get(Comment, comment.id)
   end
 
   test "updates chosen resource and redirects when data is valid and logged in as the author", %{conn: conn, user: user, post: post, comment: comment } do
@@ -36,6 +42,12 @@ defmodule Pxblog.CommentControllerTest do
     |> put(post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
     assert redirected_to(conn) == user_post_path(conn, :show, user, post)
     assert Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
+
+  test "does not update the comment when not logged in as an authorized user", %{conn: conn, post: post, comment: comment} do
+    conn = put(conn, post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
+    assert redirected_to(conn) == page_path(conn, :index)
+    refute Repo.get_by(Comment, %{id: comment.id, approved: true})
   end
 
   defp login_user(conn, user) do
