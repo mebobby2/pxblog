ea5d921e89ffcfc41056606353edf505b24915f0
Bobby Lei <bobl53@gmail.com>
2016-08-20 11:56:44 +0800

    handling messages inside comment channel

--- web/channels/comment_channel.ex
+++ web/channels/comment_channel.ex
@@ -1,7 +1,7 @@
 defmodule Pxblog.CommentChannel do
   use Pxblog.Web, :channel
 
-  def join("comment:lobby", payload, socket) do
+  def join("comments:" <> _comment_id, payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -9,16 +9,18 @@ defmodule Pxblog.CommentChannel do
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("CREATED_COMMENT", payload, socket) do
+    broadcast socket, "CREATED_COMMENT", payload
+    {:noreply, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (comment:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  def handle_in("APPROVED_COMMENT", payload, socket) do
+    broadcast socket, "APPROVED_COMMENT", payload
+    {:noreply, socket}
+  end
+
+  def handle_in("DELETED_COMMENT", payload, socket) do
+    broadcast socket, "DELETED_COMMENT", payload
     {:noreply, socket}
   end
 
